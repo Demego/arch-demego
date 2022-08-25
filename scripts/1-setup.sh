04ba5333de3765f2d3e9b36e97c51e12b1acba3c
Aivaras <aivaras@fundvest.eu>
2022-08-25 11:19:55 +0300

    up

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -91,6 +91,25 @@ if [[ ! $DESKTOP_ENV == server ]]; then
   done
 fi
 echo -ne "
+
+-------------------------------------------------------------------------
+                    Installing Dev Environment
+-------------------------------------------------------------------------
+"
+# sed $INSTALL_TYPE is using install type to check for MINIMAL installation, if it's true, stop
+# stop the script and move on, not installing any more packages below that line
+if [[ ! $DESKTOP_ENV == server ]]; then
+  sed -n '/'$INSTALL_TYPE'/q;p' $HOME/ArchTitus/pkg-files/dev-pkgs.txt | while read line
+  do
+    if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]; then
+      # If selected installation type is FULL, skip the --END OF THE MINIMAL INSTALLATION-- line
+      continue
+    fi
+    echo "INSTALLING: ${line}"
+    sudo pacman -S --noconfirm --needed ${line}
+  done
+fi
+echo -ne "
 -------------------------------------------------------------------------
                     Installing Microcode
 -------------------------------------------------------------------------
